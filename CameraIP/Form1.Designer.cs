1b08a1d94b22f0daf1b6255c52f8388b6ba5dcd1
chulan.kumara <chulan.kumara@DESKTOP-4VO3P04>
2017-09-18 16:06:55 +0530

    Add project files.

---
+++ CameraIP/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CameraIP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(591, 469);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(628, 38);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 493);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_start;
+    }
+}
+
---
+++ CameraIP/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MjpegProcessor;
+
+namespace CameraIP
+{
+    public partial class Form1 : Form
+    {
+        private MjpegDecoder mjp;
+        public Form1()
+        {
+            InitializeComponent();
+            mjp = new MjpegDecoder();
+            mjp.FrameReady += mjp_FrameReady;
+            mjp.Error += Mjp_Error;
+        }
+
+        private void Mjp_Error(object sender, MjpegProcessor.ErrorEventArgs e)
+        {
+            MessageBox.Show(e.Message);
+        }
+
+        private void mjp_FrameReady(object sender, FrameReadyEventArgs e)
+        {
+            pictureBox1.Image = e.Bitmap;
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            //mjp.ParseStream(new Uri("http://10.128.1.31/Streaming/Channels/1/picture"), "admin", "admin1234");
+            mjp.ParseStream(new Uri("http://88.53.197.250/axis-cgi/mjpg/video.cgi?resolution=320×240"));
+        }
+    }
+}
